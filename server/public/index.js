e61bf9350b2986c711c55b318e17e181bab72f23
megaconfidence <cokoghenun@gmail.com>
2024-10-17 23:37:09 +0100

    feat: Data coordination

--- server/public/index.js
+++ server/public/index.js
@@ -3,14 +3,31 @@ const powerSwitch = document.getElementById("power");
 const brightnessSlider = document.getElementById("brightness");
 const colorPicker = document.getElementById("color");
 
+const ws = new WebSocket("ws://localhost:8787");
+ws.onmessage = (e) => {
+  const msg = JSON.parse(e.data);
+  console.log(msg);
+  switch (msg.type) {
+    case "config":
+      hangleUiSync(msg.data);
+      break;
+    default:
+      break;
+  }
+};
+
+function hangleUiSync(payload) {
+  const { power, brightness, color } = payload;
+  powerSwitch.checked = power === "on" ? true : false;
+  brightnessSlider.value = brightness;
+  colorPicker.value = rgbToHex(color);
+  updateBulbUi(payload);
+}
+
 function updateBulbState() {
   const power = powerSwitch.checked ? "on" : "off";
   const brightness = brightnessSlider.value;
   const color = hexToRgb(colorPicker.value);
-
-  // Update bulb appearance
-  bulb.style.backgroundColor = power === "on" ? colorPicker.value : "#ccc";
-  bulb.style.opacity = power === "on" ? brightness / 100 : 0.5;
 
   // Prepare payload for API
   const payload = {
@@ -19,10 +36,25 @@ function updateBulbState() {
     color: color,
   };
 
+  updateBulbUi(payload);
   // Send payload to API
   sendToApi(payload);
 }
-
+function updateBulbUi({ power, color, brightness }) {
+  bulb.style.backgroundColor = power === "on" ? rgbToHex(color) : "#ccc";
+  bulb.style.opacity = power === "on" ? brightness / 100 : 0.5;
+}
+function rgbToHex(rgb) {
+  return (
+    "#" +
+    rgb
+      .map((x) => {
+        const hex = x.toString(16);
+        return hex.length === 1 ? "0" + hex : hex;
+      })
+      .join("")
+  );
+}
 function hexToRgb(hex) {
   const r = parseInt(hex.slice(1, 3), 16);
   const g = parseInt(hex.slice(3, 5), 16);
@@ -32,16 +64,19 @@ function hexToRgb(hex) {
 
 function sendToApi(payload) {
   console.log("Sending to API:", payload);
-  fetch("/api", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  })
-    .then((response) => response.json())
-    .then((data) => console.log("Success:", data))
-    .catch((error) => console.error("Error:", error));
+  if (ws.readyState === WebSocket.OPEN) {
+    ws.send(JSON.stringify({ type: "config", data: payload }));
+  }
+  // fetch("/api", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify(payload),
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => console.log("Success:", data))
+  //   .catch((error) => console.error("Error:", error));
 }
 
 // Add event listeners
